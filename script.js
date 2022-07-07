8115f7d66e83e344fa076679c518e12d530d03d6
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-07-06 23:44:12 -0300

    autocomplete

--- script.js
+++ script.js
@@ -26,3 +26,41 @@ function showAdd(){
 
     menuAdd.classList.toggle("on");
 }
+//Funções para autocomplete
+let ativo = ["ITSA4", "PSSA3", "BBDC4", "NVDC34", "BBAS3"];
+let sortedAtivos = ativo.sort();
+
+let input = document.querySelector("#input");
+
+input.addEventListener("keyup", (e) => {
+    //Apaga todos os itens caso o usuário apague ou adicione novas letras
+    removeElements();
+    //Loop no array
+    for(let i of sortedAtivos){
+        //converte o input em minúsculo e compara com o array
+        if (i.toLowerCase().startsWith(input.value.toLowerCase()) && input.value != ""){
+            //caso verdadeiro cria o elemnto li
+            let listItem = document.createElement("li");
+            // lis criados vão ter a mesma classe
+            listItem.classList.add("list-items");
+            listItem.style.cursor = "pointer";
+            listItem.setAttribute("onclick", "displayAtivos('" + i +"')");
+            //partes iguais em bold
+            let word = "<b>" + i.substr(0, input.value.length) +"</b>";
+            word += i.substr(input.value.length);
+            //Mostra o valor no array
+            listItem.innerHTML = word;
+            document.querySelector(".list").appendChild(listItem);
+        }
+    }
+});
+function displayAtivos(value){
+    input.value = value;
+    removeElements();
+}
+function removeElements(){
+    let items = document.querySelectorAll(".list-items");
+     items.forEach((item) => {
+        item.remove();
+     });
+}
